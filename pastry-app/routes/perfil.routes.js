d977fc544a5b0bb8c1118d41d8fbfea7e0bb7b39
Noelia Parada <noeliacastrelo@gmail.com>
2022-02-28 14:11:53 +0100

    rutas crud, crear y leer

--- pastry-app/routes/perfil.routes.js
+++ pastry-app/routes/perfil.routes.js
@@ -1,11 +1,46 @@
 const router = require("express").Router();
 const isLoggedIn = require("../middleware/isLoggedIn")
 const User =require("../models/User.model")
+const RecetaModel = require("../models/Receta.model")
+
 
  router.get('/',isLoggedIn,(req, res, next) =>{ 
-       console.log( "usuario autorizado") 
+       //console.log( "usuario autorizado") 
        res.render("perfil/perfil-usuario.hbs")
  })
+
+ router.get ("/crear-receta", isLoggedIn, (req, res, next) => {
+      res.render("recetas/crear-receta.hbs")
+ })
+
+ router.post ("/crear-receta", isLoggedIn, (req, res, next) => {
+      const {nombre,creacion, imagen, tipo, dificultad, autor} = req.body
+
+      RecetaModel.create({nombre,creacion, imagen, tipo, dificultad, autor})
+      .then((crearReceta) =>{
+            res.redirect(`/${crearReceta._id}/actualizar`)
+      })
+      .catch((err) => {
+          next(err);
+     })
+ })
+
+ router.get ("/:id/actualizar",isLoggedIn, (req, res, next) => {
+      const {id} = req.params
+
+      RecetaModel.findById(id) 
+      .then((recetaActualizar) => {
+           res.render("recetas/actualizar-receta.hbs", {recetaActualizar})
+      })
+      .catch((err) => {
+           next(err);
+      })
+      
+ }) 
+
+
+
+
 
 
 
